23aa5aefb70ee87d9b30c1dc9bd23c13be434d65
Charaf Mrah <charaf4charaf@gmail.com>
2022-09-08 00:12:17 +0300

    todo

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+// TODO: Add a title prop to the Title component
+const Title = () => {
+    return (
+        <div>
+            <h1>Privacy</h1>
+        </div>
+    )
+}
+
+export default Title

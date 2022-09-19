387940fb67db769b21f8d8d9ab3fa779d217099a
Charaf Mrah <charaf4charaf@gmail.com>
2022-09-19 17:23:19 +0300

    conflict removed

--- src/routes/TitleGenerator/TitleSearch.js
+++ src/routes/TitleGenerator/TitleSearch.js
@@ -9,7 +9,7 @@ const TitleSearch = () => {
             id="description"
             name="description"
             type={"text"}
-            className="mt-1 block p-2 w-full rounded-md border-gray-300 shadow-sm focus:border-x-primary focus:ring-primary"
+            className="block w-full p-2 mt-1 border-gray-300 rounded-md shadow-sm focus:border-x-primary focus:ring-primary"
             placeholder="Lush green grass, blue sky, and a beautiful lake."
             defaultValue={""}
           />
@@ -21,7 +21,7 @@ const TitleSearch = () => {
           </p>
           <button
             type="submit"
-            className="inline-flex justify-center rounded-md border border-transparent bg-primary hover:bg-white py-2 px-4 text-sm font-medium text-white hover:text-primary shadow-sm hover:bg-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+            className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary hover:bg-white hover:text-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
           >
             Generate
           </button>

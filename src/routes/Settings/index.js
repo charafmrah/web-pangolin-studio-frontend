653c772453fa9c9a667681853e4ae80bcc68bac5
Charaf Mrah <charaf4charaf@gmail.com>
2022-10-24 23:07:02 +0300

    update

--- src/routes/Settings/index.js
+++ src/routes/Settings/index.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Toggle from "../../components/Toggle";
 
 const Settings = () => {
   return (
     <div>
       <h1>Settings</h1>
-
-      <Toggle />
     </div>
   );
 };

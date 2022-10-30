edc63b8f7ef5ec4e0e58140e10e12d1702a99c5e
Charaf Mrah <charaf4charaf@gmail.com>
2022-10-30 13:31:04 +0300

    refactor

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import { auth } from "../app/firebase";
+import { auth } from "../utils/firebase";
 
 const AuthContext = React.createContext();
 

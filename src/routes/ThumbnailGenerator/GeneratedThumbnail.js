51dbbbb6714463fb70611f8d31daacc9ddad5f8a
Charaf Mrah <charaf4charaf@gmail.com>
2022-11-04 16:46:46 +0300

    refactor

--- src/routes/ThumbnailGenerator/GeneratedThumbnail.js
+++ src/routes/ThumbnailGenerator/GeneratedThumbnail.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const GeneratedThumbnail = ({ image, loading }) => {
+  async function handleSave() {
+    const storage = getStorage();
+    const storageRef = ref(storage, image);
+    uploadBytes(storageRef, image).then((snapshot) => {
+      console.log("Uploaded a blob or file!");
+    });
+  }
+
   return (
     <div className="mt-4 text-center">
       <img className="w-full" alt="" src={image}></img>
 
       {image !== "" && (
-        <button className="inline-flex justify-center w-full px-4 py-2 mt-2 text-sm font-medium text-black hover:text-white border border-transparent rounded-md shadow-sm bg-slate-200 hover:bg-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+        <button
+          className="inline-flex justify-center w-full px-4 py-2 mt-2 text-sm font-medium text-black hover:text-white border border-transparent rounded-md shadow-sm bg-slate-200 hover:bg-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+          onClick={handleSave}
+        >
           Save
         </button>
       )}
--- src/routes/ThumbnailGenerator/ThumbnailSearch.js
+++ src/routes/ThumbnailGenerator/ThumbnailSearch.js
@@ -14,11 +14,9 @@ const ThumbnailSearch = () => {
       mode: "cors",
       cache: "default",
     };
-    let url = "http://127.0.0.1:8000/thumbnail/?prompt=" + prompt;
+    let url = process.env.REACT_APP_DJANGO_URL + "thumbnail/?prompt=" + prompt;
     const res = await fetch(url, config).then((res) => res.blob());
-    console.log(res);
     const imageObjectURL = URL.createObjectURL(res);
-    console.log(imageObjectURL);
     return imageObjectURL;
   };
 
